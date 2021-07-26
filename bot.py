99fd051af9ba9b2b136a7cd066f1bd1b4abc98eb
MarbleKolya <mr.marbel@mail.ru>
2021-07-26 17:44:38 +0300

    Comment

--- bot.py
+++ bot.py
@@ -19,8 +19,6 @@ def welcome(message):
         PRIMARY KEY(id)
     )""")
     connect.commit()
-
-
     #обработка имени и фамилии
     if message.text == '/start':
         bot.send_message(message.chat.id, "Добро пожаловать, {0.first_name}!\nЯ - <b>{1.first_name}</b>, бот созданный чтобы пройти курс по бережливому производству.".format(message.from_user, bot.get_me()), parse_mode='html')
@@ -46,7 +44,6 @@ def get_surname(message, name): #получаем фамилию
 
 def user_answer(message,name,surname):
     if message.text == "Да":
-        bot.send_message(message.chat.id, 'Обработка да'+name+surname)
         connect = sqlite3.connect('KompoDB.db')
         cursor = connect.cursor()
         people_id = message.from_user.id
@@ -65,15 +62,37 @@ def user_answer(message,name,surname):
             connect.commit()
             bot.send_message(message.chat.id, 'Запомню : )', reply_markup=markup)
         else:
-           bot.send_message(message.chat.id, 'На ваше телеграмм id заригистрирован пользователь', reply_markup=markup)
+           bot.send_message(message.chat.id, 'На ваше телеграмм id зарегистрирован пользователь', reply_markup=markup)
     elif message.text =="Нет":
         bot.send_message(message.chat.id, 'Введите корректное имя: ')
         bot.register_next_step_handler(message, get_name);
 
+def test(message):
+    bot.send_message(message.chat.id, "Типа теста" + str(message))
+
 @bot.callback_query_handler(func=lambda call: True)
 
 def callback_worker(call):
-    if call.data == 'phone':
+    connect = sqlite3.connect('KompoDB.db')
+    cursor = connect.cursor()
+    if call.data == 'test1':
+        for i in range(10):
+            question_arr = cursor.execute('SELECT question FROM test_question WHERE question_number == ?', (i+1,)).fetchone()
+            question = question_arr[0]
+            markup = types.InlineKeyboardMarkup(row_width=3)
+            key1 = types.InlineKeyboardButton("1", callback_data='1')
+            key2 = types.InlineKeyboardButton("2", callback_data='2')
+            key3 = types.InlineKeyboardButton("3", callback_data='3')
+            markup.add(key1, key2, key3)
+            bot.send_message(call.message.chat.id, "Вопрос " + str(i+1)+ ":\n" + question , reply_markup=markup)
+            #answer_arr = cursor.execute('SELECT right_answer FROM test_question WHERE question_number == ?', (i+1,)).fetchone()
+            #answer = answer_arr[0]
+            #bot.register_next_step_handler(call.message.chat.id, answer)
+            #if (call.data == str(answer)):
+            #    bot.send_message(call.message.chat.id, "Правильный ответ")
+            #else:
+            #    bot.send_message(call.message.chat.id, "Неправильный ответ")
+    elif call.data == 'phone':
         bot.send_message(call.message.chat.id, '+375339113030')
     elif call.data == 'student':
         markup = types.InlineKeyboardMarkup(row_width=2)
@@ -88,59 +107,33 @@ def callback_worker(call):
         bot.send_message(call.message.chat.id, 'Выберите рабочее место:', reply_markup=markup)
     elif call.data == 'machineproduct':
         markup = types.InlineKeyboardMarkup(row_width=2)
-        key1 = types.InlineKeyboardButton("Метода1", callback_data='metoda1')
-        key2 = types.InlineKeyboardButton("Тест1", callback_data='test1')
-        key3 = types.InlineKeyboardButton("Метода2", callback_data='metoda2')
-        key4 = types.InlineKeyboardButton("Тест2", callback_data='test2')
-        markup.add(key1, key2, key3, key4)
-        bot.send_message(call.message.chat.id, 'Выберите тест для прохождения: ', reply_markup=markup)
+        key1 = types.InlineKeyboardButton("Метода1", callback_data='metoda11')
+        key2 = types.InlineKeyboardButton("Тест1", callback_data='test11')
+        key3 = types.InlineKeyboardButton("Метода2", callback_data='metoda12')
+        key4 = types.InlineKeyboardButton("Тест2", callback_data='test12')
+        markup.add(key1, key2, key3,key4)
+        msg = bot.send_message(call.message.chat.id, 'Выберите тест для прохождения: ', reply_markup=markup)
+        bot.register_next_step_handler(msg, test)
     elif call.data == 'testStudent':
         markup = types.InlineKeyboardMarkup(row_width=2)
-        key1 = types.InlineKeyboardButton("Методичка 1", callback_data='metoda1')
-        key2 = types.InlineKeyboardButton("Тест 1", callback_data='test1')
-        key3 = types.InlineKeyboardButton("Методичка 2", callback_data='metoda2')
-        key4 = types.InlineKeyboardButton("Тест 2 ", callback_data='test2')
-        markup.add(key1, key2, key3, key4)
+        key1 = types.InlineKeyboardButton("Методичка Студента", callback_data='metoda1')
+        key2 = types.InlineKeyboardButton("Тест Студента", callback_data='test1')
+        markup.add(key1, key2)
         bot.send_message(call.message.chat.id, 'Выберите тест для прохождения: ', reply_markup=markup)
     elif call.data == 'office':
         markup = types.InlineKeyboardMarkup(row_width=2)
-        key1 = types.InlineKeyboardButton("Методичка 1", callback_data='metoda1')
+        key1 = types.InlineKeyboardButton("Методичка 1", callback_data='metoda1', url="https://drive.google.com/drive/folders/1SrpXFa2VA6w5z7PCr084Md4zEkXOpUQD?usp=sharing")
         key2 = types.InlineKeyboardButton("Тест 1", callback_data='test1')
-        key3 = types.InlineKeyboardButton("Методичка 2", callback_data='metoda2')
+        key3 = types.InlineKeyboardButton("Методичка 2", callback_data='metoda2', url="https://drive.google.com/drive/folders/1TA-wpMNR2_JUb8zl3E9Eg3SIG7QX_pGl?usp=sharing")
         key4 = types.InlineKeyboardButton("Тест 2", callback_data='test2')
-        key5 = types.InlineKeyboardButton("Методичка 3", callback_data='metoda3')
+        key5 = types.InlineKeyboardButton("Методичка 3", callback_data='metoda3', url="https://drive.google.com/drive/folders/1TVClZ4pPA3Z7t70vF1QXR_hMPpdEd1Dp?usp=sharing")
         key6 = types.InlineKeyboardButton("Тест 3", callback_data='test3')
-        key7 = types.InlineKeyboardButton("Методичка 4", callback_data='metoda4')
-        key8 = types.InlineKeyboardButton("Методичка 5", callback_data='metoda5')
+        key7 = types.InlineKeyboardButton("Методичка 4", callback_data='metoda4', url="https://drive.google.com/drive/folders/1TZUzOpKV_SFo6UXPlwHZgAUh3Pt8gYyu?usp=sharing")
+        key8 = types.InlineKeyboardButton("Тест 4", callback_data='test4')
         markup.add(key1, key2, key3, key4, key5, key6, key7, key8)
         bot.send_message(call.message.chat.id, 'Выберите тест для прохождения: ', reply_markup=markup)
     elif call.data == 'result':
-        bot.send_message(call.message.chat.id, 'Курс: \n Номер теста: \n 1: 9/10 \n 2: \n 3: \n Проходной балл: 100 \n Ваш балл: 9  ')
-
-    elif call.data == 'test1':
-        for i in range(10):
-            connect = sqlite3.connect('KompoDB.db')
-            cursor = connect.cursor()
-            question_arr = cursor.execute('SELECT question FROM test_question WHERE question_number == ?', (i+1,)).fetchone()
-            question = question_arr[0]
-
-            markup = types.InlineKeyboardMarkup(row_width=3)
-            key1 = types.InlineKeyboardButton("1", callback_data='1')
-            key2 = types.InlineKeyboardButton("2", callback_data='2')
-            key3 = types.InlineKeyboardButton("3", callback_data='3')
-            markup.add(key1, key2, key3)
-            bot.send_message(call.message.chat.id, "Вопрос " + str(i+1)+ ":\n" + question , reply_markup=markup)
-
-            #answer_arr = cursor.execute('SELECT right_answer FROM test_question WHERE question_number == ?', (i+1,)).fetchone()
-            #answer = answer_arr[0]
-            #bot.register_next_step_handler(call.message.chat.id, answer)
-
-            #if (call.data == str(answer)):
-            #    bot.send_message(call.message.chat.id, "Правильный ответ")
-            #else:
-            #    bot.send_message(call.message.chat.id, "Неправильный ответ")
-
-
+        bot.send_message(call.message.chat.id, 'Курс: Бережливое производство\n Номер теста: \n 1: 9/10 \n 2: \n 3: \n Проходной балл: 100 \n Ваш балл: 9  ')
     elif call.data == 'test2':
         bot.send_message(call.message.chat.id, '😉 Тут скоро будет тест 2')
     elif call.data == 'metoda1':
@@ -151,7 +144,6 @@ def callback_worker(call):
         bot.send_message(call.message.chat.id, 'Методичка 2 '+ '\n' +
          'Надо, что бы люди относились к своим рабочим местам как к своему личному дому на берегу моря, где каждый квадратный сантиметр стоит целое состояние. Как только вы добьетесь этого сдвига в восприятии, каждый работник начнет смотреть на процесс совсем другими глазами '+ '\n' +
          '5S – это не уборка, это метод организации рабочего пространства. Это то, как вы видите свою работу.')
-
 
 @bot.message_handler(content_types=['text'])
 def lalala(message):
@@ -164,14 +156,12 @@ def lalala(message):
             markup.add(keyWorker, keyStudend,keyResult)
             bot.send_message(message.chat.id, 'На прохождение тестов дается бесконечное количество времени и попыток, но в зачот идет только первое прохождение тестов. Если Вы являетесь сотрудником Компо, то выберите соответствующую кнопку. Если Вы только начинаетв обучение, то выберите кнопку студент:', reply_markup=markup)
         elif message.text == '⚙️ Настройки':
-
             markup = types.InlineKeyboardMarkup(row_width=2)
             keyResName = types.InlineKeyboardButton("🙅‍♂️ Сбросить имя", callback_data='resName')
             keyTechHelp = types.InlineKeyboardButton("🆘 Тех. поддержка", callback_data='techHelp', url ="https://t.me/Ros_Mic")
             markup.add(keyResName, keyTechHelp)
             bot.send_message(message.chat.id, 'В настройках Вы можете изменить своё имя и написать в тех. поддержку', reply_markup=markup)
         elif message.text == '📚 Лекции':
-
             markup = types.InlineKeyboardMarkup(row_width=2)
             item1 = types.InlineKeyboardButton("📖 Лекции", callback_data='lecs', url="https://drive.google.com/drive/folders/1CTXTahfl6nSvhXh61aYB5mI1IyKDArT6?usp=sharing")
             markup.add(item1)
