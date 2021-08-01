05639d2868f6238677aed08987b2a1dee12bbf3f
MarbleKolya <mr.marbel@mail.ru>
2021-08-01 20:58:55 +0300

    Work wich test_number

--- bot.py
+++ bot.py
@@ -18,7 +18,7 @@ def welcome(message):
         PRIMARY KEY(id)
     )""")
     connect.commit()
-    #обработка имени и фамилии
+    #регистрация, обработка имени и фамилии
     if message.text == '/start':
         bot.send_message(message.chat.id, "Добро пожаловать, {0.first_name}!\nЯ - <b>{1.first_name}</b>, бот созданный чтобы пройти курс по бережливому производству.".format(message.from_user, bot.get_me()), parse_mode='html')
         bot.send_message(message.from_user.id, "Введите ваше настоящее имя :");
@@ -71,6 +71,8 @@ def user_answer(message,name,surname):
 
 
 def test_number(message):
+    test_number = message.text
+    bot.send_message(message.chat.id, test_number)
     connect = sqlite3.connect('KompoDB.db')
     cursor = connect.cursor()
     question_arr = cursor.execute('SELECT question FROM test_question WHERE test == ? ORDER BY question_number', (message.text)).fetchall()
@@ -82,46 +84,60 @@ def test_number(message):
     msg = bot.send_message(message.chat.id, "На прохождение тестов дается бесконечное количество времени и попыток, но в зачёт идет только первое прохождение теста.  ", reply_markup=keyboard)
     number_question = 0
     score = 0
-    bot.register_next_step_handler(msg, test, question_arr , answer_arr, number_question,score)
+    #bot.register_next_step_handler(msg, test ,test_number, question_arr , answer_arr, number_question,score)
     #test(msg, question_arr , answer_arr, number_question,score)
 
 
-def test(message, question_arr , answer_arr, number_question, score):
+def test(message, test_number, question_arr , answer_arr, number_question, score):
     if int(number_question)<len(question_arr):
         true_answer = answer_arr[number_question]
         keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=3);
         keyboard.add(types.KeyboardButton("1"),types.KeyboardButton("2"),types.KeyboardButton("3"));
         bot.send_message(message.chat.id,"Вопрос " + str(number_question+1))
         message = bot.send_message(message.chat.id, question_arr[number_question], reply_markup=keyboard)
-        bot.register_next_step_handler(message,answer,question_arr,answer_arr, number_question, score, true_answer)
+        bot.register_next_step_handler(message,answer,test_number, question_arr,answer_arr, number_question, score, true_answer)
     else:
+        connect = sqlite3.connect('KompoDB.db')
+        cursor = connect.cursor()
+        people_id = message.from_user.id
+        result = [people_id, test_number, score]
+        cursor.execute("INSERT INTO users_result VALUES(?,?,?);", result)
+        connect.commit()
+
+
         markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2, selective=False)
         item1 = types.KeyboardButton("❓ Тест")
         item2 = types.KeyboardButton("⚙️ Настройки")
         item3 = types.KeyboardButton("📚 Лекции")
         item4 = types.KeyboardButton("📞 Контакты")
         markup.add(item1, item3, item4, item2)
-        msg = bot.send_message(message.chat.id, "Тест закончен \n Ваш балл: " + str(score), reply_markup=markup)
+        msg = bot.send_message(message.chat.id, "Тест " +str(test_number) +" закончен \n Ваш балл: " + str(score), reply_markup=markup)
         #connect = sqlite3.connect('KompoDB.db')
 
-def answer(message,question_arr,answer_arr, number_question, score, true_answer):
+def answer(message,test_number, question_arr,answer_arr, number_question, score, true_answer):
+
     mt = str(message.text)
     ta = str(true_answer[0])
     if message.text == "1":
         if mt == ta:
             score += 1
         number_question += 1
-        test(message, question_arr , answer_arr, number_question, score)
+        test(message, test_number, question_arr , answer_arr, number_question, score)
     elif message.text == "2":
         if mt == ta:
             score += 1
         number_question += 1
-        test(message, question_arr , answer_arr, number_question, score)
+        test(message, test_number, question_arr , answer_arr, number_question, score)
     elif message.text == "3":
         if mt == ta:
             score += 1
         number_question += 1
-        test(message, question_arr , answer_arr, number_question, score)
+        test(message, test_number, question_arr , answer_arr, number_question, score)
+    else:
+        #обратотка не кнопки
+        msg = bot.send_message(message.chat.id, "Введите вариант ответа 1, 2 или 3")
+        bot.register_next_step_handler(msg,answer,test_number, question_arr,answer_arr, number_question, score, true_answer)
+
 
 
     #msg = bot.register_next_step_handler(user_answer,test,question_arr,answer_arr, number_question, score)
@@ -139,7 +155,7 @@ def answer(message,question_arr,answer_arr, number_question, score, true_answer)
 def callback_worker(call):
     connect = sqlite3.connect('KompoDB.db')
     cursor = connect.cursor()
-    if call.data == 'test1':
+    if call.data == 'test':
         #Сделать обратотку номера теста
         number = bot.send_message(call.message.chat.id, "Напиши номер теста")
         bot.register_next_step_handler(number, test_number)
@@ -159,13 +175,34 @@ def callback_worker(call):
         bot.send_message(call.message.chat.id, 'Выберите рабочее место:', reply_markup=markup)
     elif call.data == 'machineproduct':
         markup = types.InlineKeyboardMarkup(row_width=2)
-        key1 = types.InlineKeyboardButton("Часть 1", callback_data='metoda11')
-        key2 = types.InlineKeyboardButton("Тест 1", callback_data='test11')
-        key3 = types.InlineKeyboardButton("Часть 2", callback_data='metoda12')
-        key4 = types.InlineKeyboardButton("Тест 2", callback_data='test12')
-        markup.add(key1, key2, key3,key4)
+        key1 = types.InlineKeyboardButton("Часть 1", callback_data='metoda1', url="https://drive.google.com/drive/folders/1SrpXFa2VA6w5z7PCr084Md4zEkXOpUQD?usp=sharing")
+        key2 = types.InlineKeyboardButton("Тест 1", callback_data='test1')
+        key3 = types.InlineKeyboardButton("Часть 2", callback_data='metoda2', url="https://drive.google.com/drive/folders/1TA-wpMNR2_JUb8zl3E9Eg3SIG7QX_pGl?usp=sharing")
+        key4 = types.InlineKeyboardButton("Тест 2", callback_data='test1')
+        key5 = types.InlineKeyboardButton("Часть 3", callback_data='metoda3', url="https://drive.google.com/drive/folders/1TVClZ4pPA3Z7t70vF1QXR_hMPpdEd1Dp?usp=sharing")
+        key6 = types.InlineKeyboardButton("Тест 3", callback_data='test1')
+        key7 = types.InlineKeyboardButton("Часть 4", callback_data='metoda4', url="https://drive.google.com/drive/folders/1TZUzOpKV_SFo6UXPlwHZgAUh3Pt8gYyu?usp=sharing")
+        key8 = types.InlineKeyboardButton("Тест 4", callback_data='test1')
+        key9 = types.InlineKeyboardButton("Часть 5", callback_data='metoda1', url="https://drive.google.com/drive/folders/1SrpXFa2VA6w5z7PCr084Md4zEkXOpUQD?usp=sharing")
+        key10 = types.InlineKeyboardButton("Тест 5", callback_data='test1')
+        key11 = types.InlineKeyboardButton("Часть 6", callback_data='metoda2', url="https://drive.google.com/drive/folders/1TA-wpMNR2_JUb8zl3E9Eg3SIG7QX_pGl?usp=sharing")
+        key12 = types.InlineKeyboardButton("Тест 6", callback_data='test2')
+        key13 = types.InlineKeyboardButton("Часть 7", callback_data='metoda3', url="https://drive.google.com/drive/folders/1TVClZ4pPA3Z7t70vF1QXR_hMPpdEd1Dp?usp=sharing")
+        key14 = types.InlineKeyboardButton("Тест 7", callback_data='test3')
+        key15 = types.InlineKeyboardButton("Часть 8", callback_data='metoda4', url="https://drive.google.com/drive/folders/1TZUzOpKV_SFo6UXPlwHZgAUh3Pt8gYyu?usp=sharing")
+        key16 = types.InlineKeyboardButton("Тест 8", callback_data='test4')
+        key17 = types.InlineKeyboardButton("Часть 9", callback_data='metoda1', url="https://drive.google.com/drive/folders/1SrpXFa2VA6w5z7PCr084Md4zEkXOpUQD?usp=sharing")
+        key18 = types.InlineKeyboardButton("Тест 9", callback_data='test1')
+        key19 = types.InlineKeyboardButton("Часть 10", callback_data='metoda2', url="https://drive.google.com/drive/folders/1TA-wpMNR2_JUb8zl3E9Eg3SIG7QX_pGl?usp=sharing")
+        key20 = types.InlineKeyboardButton("Тест 10", callback_data='test2')
+        key21 = types.InlineKeyboardButton("Часть 11", callback_data='metoda3', url="https://drive.google.com/drive/folders/1TVClZ4pPA3Z7t70vF1QXR_hMPpdEd1Dp?usp=sharing")
+        key22 = types.InlineKeyboardButton("Тест 11", callback_data='test3')
+        key23 = types.InlineKeyboardButton("Часть 12", callback_data='metoda4', url="https://drive.google.com/drive/folders/1TZUzOpKV_SFo6UXPlwHZgAUh3Pt8gYyu?usp=sharing")
+        key24 = types.InlineKeyboardButton("Тест 12", callback_data='test4')
+        markup.add(key1, key2, key3, key4, key5, key6, key7, key8, key9, key10,key11,key12,key13,key14,key15,key16,key17,key18,key19,key20,key21,key22,key23,key24)
         msg = bot.send_message(call.message.chat.id, 'Выберите тест для прохождения: ', reply_markup=markup)
-        bot.register_next_step_handler(msg, test)
+        bot.register_next_step_handler(msg, test_number)
+        #bot.register_next_step_handler(msg, test)
     elif call.data == 'testStudent':
         markup = types.InlineKeyboardMarkup(row_width=2)
         key1 = types.InlineKeyboardButton("Методичка Студента", callback_data='metoda1')
@@ -188,14 +225,7 @@ def callback_worker(call):
         bot.send_message(call.message.chat.id, 'Курс: Бережливое производство\n Номер теста: \n 1: 9/10 \n 2: \n 3: \n Проходной балл: 100 \n Ваш балл: 9  ')
     elif call.data == 'test2':
         bot.send_message(call.message.chat.id, '😉 Тут скоро будет тест 2')
-    elif call.data == 'metoda1':
-        bot.send_message(call.message.chat.id, 'Методичка 1 '+ '\n' +
-         'После упорной работы по внедрению системы 5S ,наблюдая улучшения, сотрудники начинают говорить: «Мы сделали это!» Они могут расслабиться, успокоиться на некоторое время (или, что еще хуже, вообще опустить руки). '+ '\n' +
-         'Существуют силы, которые пытаются возвратить ситуацию к прежнему состоянию, поэтому руководству крайне важно создать систему, обеспечивающую непрерывность реализации пяти S.')
-    elif call.data == 'metoda2':
-        bot.send_message(call.message.chat.id, 'Методичка 2 '+ '\n' +
-         'Надо, что бы люди относились к своим рабочим местам как к своему личному дому на берегу моря, где каждый квадратный сантиметр стоит целое состояние. Как только вы добьетесь этого сдвига в восприятии, каждый работник начнет смотреть на процесс совсем другими глазами '+ '\n' +
-         '5S – это не уборка, это метод организации рабочего пространства. Это то, как вы видите свою работу.')
+
 
 @bot.message_handler(content_types=['text'])
 def lalala(message):
