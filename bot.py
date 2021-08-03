91e133171652bc870f87a8124338be8bcee5ecd9
MarbleKolya <mr.marbel@mail.ru>
2021-08-03 22:56:25 +0300

    Almost 1.0

--- bot.py
+++ bot.py
@@ -69,25 +69,6 @@ def user_answer(message,name,surname):
 
 #####################################################################################
 
-
-def test_number(message):
-    test_number = message.text
-    bot.send_message(message.chat.id, test_number)
-    connect = sqlite3.connect('KompoDB.db')
-    cursor = connect.cursor()
-    question_arr = cursor.execute('SELECT question FROM test_question WHERE test == ? ORDER BY question_number', (message.text)).fetchall()
-    answer_arr = cursor.execute('SELECT right_answer FROM test_question WHERE test == ? ORDER BY question_number', (message.text)).fetchall()
-
-    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=1);
-    keyboard.add(types.KeyboardButton("Начать"))
-
-    msg = bot.send_message(message.chat.id, "На прохождение тестов дается бесконечное количество времени и попыток, но в зачёт идет только первое прохождение теста.  ", reply_markup=keyboard)
-    number_question = 0
-    score = 0
-    #bot.register_next_step_handler(msg, test ,test_number, question_arr , answer_arr, number_question,score)
-    #test(msg, question_arr , answer_arr, number_question,score)
-
-
 def test(message, test_number, question_arr , answer_arr, number_question, score):
     if int(number_question)<len(question_arr):
         true_answer = answer_arr[number_question]
@@ -101,9 +82,9 @@ def test(message, test_number, question_arr , answer_arr, number_question, score
         cursor = connect.cursor()
         people_id = message.from_user.id
         result = [people_id, test_number, score]
+        # обработка на повторяящийся результат теста
         cursor.execute("INSERT INTO users_result VALUES(?,?,?);", result)
         connect.commit()
-
 
         markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2, selective=False)
         item1 = types.KeyboardButton("❓ Тест")
@@ -112,10 +93,9 @@ def test(message, test_number, question_arr , answer_arr, number_question, score
         item4 = types.KeyboardButton("📞 Контакты")
         markup.add(item1, item3, item4, item2)
         msg = bot.send_message(message.chat.id, "Тест " +str(test_number) +" закончен \n Ваш балл: " + str(score), reply_markup=markup)
-        #connect = sqlite3.connect('KompoDB.db')
+
 
 def answer(message,test_number, question_arr,answer_arr, number_question, score, true_answer):
-
     mt = str(message.text)
     ta = str(true_answer[0])
     if message.text == "1":
@@ -134,33 +114,38 @@ def answer(message,test_number, question_arr,answer_arr, number_question, score,
         number_question += 1
         test(message, test_number, question_arr , answer_arr, number_question, score)
     else:
-        #обратотка не кнопки
         msg = bot.send_message(message.chat.id, "Введите вариант ответа 1, 2 или 3")
         bot.register_next_step_handler(msg,answer,test_number, question_arr,answer_arr, number_question, score, true_answer)
-
-
-
-    #msg = bot.register_next_step_handler(user_answer,test,question_arr,answer_arr, number_question, score)
-    #test(message, question_arr , answer_arr, number_question, score)
-
-
-    #cursor = connect.cursor()
-    #people_id =  message.from_user.id
-    #result =[people_id, test_number, score]
-
-    #cursor.execute("INSERT INTO users_result VALUES(?,?,?);", result)
 
 @bot.callback_query_handler(func=lambda call: True)
 
 def callback_worker(call):
     connect = sqlite3.connect('KompoDB.db')
     cursor = connect.cursor()
-    if call.data == 'test':
-        #Сделать обратотку номера теста
-        number = bot.send_message(call.message.chat.id, "Напиши номер теста")
-        bot.register_next_step_handler(number, test_number)
-
-    elif call.data == 'phone':
+    for i in range(20):
+        if call.data == 'test'+str(i):
+            bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text='Тест выбран')
+            test_number = str(i)
+            bot.send_message(call.message.chat.id,test_number)
+            connect = sqlite3.connect('KompoDB.db')
+            cursor = connect.cursor()
+            question_arr = cursor.execute('SELECT question FROM test_question WHERE test == ? ORDER BY question_number', (test_number,)).fetchall()
+            answer_arr = cursor.execute('SELECT right_answer FROM test_question WHERE test == ? ORDER BY question_number', (test_number,)).fetchall()
+            keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=1);
+            keyboard.add(types.KeyboardButton("Начать"))
+            msg = bot.send_message(call.message.chat.id, "На прохождение тестов дается бесконечное количество времени и попыток, но в зачёт идет только первое прохождение теста.  ", reply_markup=keyboard)
+            number_question = 0
+            score = 0
+            bot.register_next_step_handler(msg, test ,test_number, question_arr , answer_arr, number_question,score)
+            #keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=1);
+            #markup = types.InlineKeyboardMarkup(row_width=2)
+            #key1 = types.InlineKeyboardButton("Часть "+str(i),url=url_arr[i-1])
+            #markup.add(key1)
+            #bot.send_message(call.message.chat.id, "Для успешного прохождения теста изучите материалы: ", reply_markup=markup)
+            #keyboard.add(types.KeyboardButton(str(i)))
+            #klc = bot.send_message(call.message.chat.id, "После успешного изучения материалов нажмите кнопку ниже 👇🏻",reply_markup=keyboard)
+            #bot.register_next_step_handler(klc, test_number)
+    if call.data == 'phone':
         bot.send_message(call.message.chat.id, '+375339113030')
     elif call.data == 'student':
         markup = types.InlineKeyboardMarkup(row_width=2)
@@ -176,32 +161,32 @@ def callback_worker(call):
     elif call.data == 'machineproduct':
         markup = types.InlineKeyboardMarkup(row_width=2)
         key1 = types.InlineKeyboardButton("Часть 1", callback_data='metoda1', url="https://drive.google.com/drive/folders/1SrpXFa2VA6w5z7PCr084Md4zEkXOpUQD?usp=sharing")
-        key2 = types.InlineKeyboardButton("Тест 1", callback_data='test1')
+        key2 = types.InlineKeyboardButton("Тест 1", callback_data='test5')
         key3 = types.InlineKeyboardButton("Часть 2", callback_data='metoda2', url="https://drive.google.com/drive/folders/1TA-wpMNR2_JUb8zl3E9Eg3SIG7QX_pGl?usp=sharing")
-        key4 = types.InlineKeyboardButton("Тест 2", callback_data='test1')
+        key4 = types.InlineKeyboardButton("Тест 2", callback_data='test6')
         key5 = types.InlineKeyboardButton("Часть 3", callback_data='metoda3', url="https://drive.google.com/drive/folders/1TVClZ4pPA3Z7t70vF1QXR_hMPpdEd1Dp?usp=sharing")
-        key6 = types.InlineKeyboardButton("Тест 3", callback_data='test1')
+        key6 = types.InlineKeyboardButton("Тест 3", callback_data='test7')
         key7 = types.InlineKeyboardButton("Часть 4", callback_data='metoda4', url="https://drive.google.com/drive/folders/1TZUzOpKV_SFo6UXPlwHZgAUh3Pt8gYyu?usp=sharing")
-        key8 = types.InlineKeyboardButton("Тест 4", callback_data='test1')
+        key8 = types.InlineKeyboardButton("Тест 4", callback_data='test8')
         key9 = types.InlineKeyboardButton("Часть 5", callback_data='metoda1', url="https://drive.google.com/drive/folders/1SrpXFa2VA6w5z7PCr084Md4zEkXOpUQD?usp=sharing")
-        key10 = types.InlineKeyboardButton("Тест 5", callback_data='test1')
+        key10 = types.InlineKeyboardButton("Тест 5", callback_data='test9')
         key11 = types.InlineKeyboardButton("Часть 6", callback_data='metoda2', url="https://drive.google.com/drive/folders/1TA-wpMNR2_JUb8zl3E9Eg3SIG7QX_pGl?usp=sharing")
-        key12 = types.InlineKeyboardButton("Тест 6", callback_data='test2')
+        key12 = types.InlineKeyboardButton("Тест 6", callback_data='test10')
         key13 = types.InlineKeyboardButton("Часть 7", callback_data='metoda3', url="https://drive.google.com/drive/folders/1TVClZ4pPA3Z7t70vF1QXR_hMPpdEd1Dp?usp=sharing")
-        key14 = types.InlineKeyboardButton("Тест 7", callback_data='test3')
+        key14 = types.InlineKeyboardButton("Тест 7", callback_data='test11')
         key15 = types.InlineKeyboardButton("Часть 8", callback_data='metoda4', url="https://drive.google.com/drive/folders/1TZUzOpKV_SFo6UXPlwHZgAUh3Pt8gYyu?usp=sharing")
-        key16 = types.InlineKeyboardButton("Тест 8", callback_data='test4')
+        key16 = types.InlineKeyboardButton("Тест 8", callback_data='test12')
         key17 = types.InlineKeyboardButton("Часть 9", callback_data='metoda1', url="https://drive.google.com/drive/folders/1SrpXFa2VA6w5z7PCr084Md4zEkXOpUQD?usp=sharing")
-        key18 = types.InlineKeyboardButton("Тест 9", callback_data='test1')
+        key18 = types.InlineKeyboardButton("Тест 9", callback_data='test13')
         key19 = types.InlineKeyboardButton("Часть 10", callback_data='metoda2', url="https://drive.google.com/drive/folders/1TA-wpMNR2_JUb8zl3E9Eg3SIG7QX_pGl?usp=sharing")
-        key20 = types.InlineKeyboardButton("Тест 10", callback_data='test2')
+        key20 = types.InlineKeyboardButton("Тест 10", callback_data='test14')
         key21 = types.InlineKeyboardButton("Часть 11", callback_data='metoda3', url="https://drive.google.com/drive/folders/1TVClZ4pPA3Z7t70vF1QXR_hMPpdEd1Dp?usp=sharing")
-        key22 = types.InlineKeyboardButton("Тест 11", callback_data='test3')
+        key22 = types.InlineKeyboardButton("Тест 11", callback_data='test15')
         key23 = types.InlineKeyboardButton("Часть 12", callback_data='metoda4', url="https://drive.google.com/drive/folders/1TZUzOpKV_SFo6UXPlwHZgAUh3Pt8gYyu?usp=sharing")
-        key24 = types.InlineKeyboardButton("Тест 12", callback_data='test4')
+        key24 = types.InlineKeyboardButton("Тест 12", callback_data='test16')
         markup.add(key1, key2, key3, key4, key5, key6, key7, key8, key9, key10,key11,key12,key13,key14,key15,key16,key17,key18,key19,key20,key21,key22,key23,key24)
-        msg = bot.send_message(call.message.chat.id, 'Выберите тест для прохождения: ', reply_markup=markup)
-        bot.register_next_step_handler(msg, test_number)
+        bot.send_message(call.message.chat.id, 'Выберите тест для прохождения: ', reply_markup=markup)
+        #bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text='Тест выбран')
         #bot.register_next_step_handler(msg, test)
     elif call.data == 'testStudent':
         markup = types.InlineKeyboardMarkup(row_width=2)
@@ -223,10 +208,6 @@ def callback_worker(call):
         bot.send_message(call.message.chat.id, 'Выберите тест для прохождения: ', reply_markup=markup)
     elif call.data == 'result':
         bot.send_message(call.message.chat.id, 'Курс: Бережливое производство\n Номер теста: \n 1: 9/10 \n 2: \n 3: \n Проходной балл: 100 \n Ваш балл: 9  ')
-    elif call.data == 'test2':
-        bot.send_message(call.message.chat.id, '😉 Тут скоро будет тест 2')
-
-
 @bot.message_handler(content_types=['text'])
 def lalala(message):
      if message.chat.type == 'private':
@@ -259,4 +240,106 @@ def lalala(message):
             markup.add(item1, item2, item3, item4)
             bot.send_message(message.chat.id, 'Здесь Вы можете свзаться с нами', reply_markup=markup)
 
+        elif message.text == '/Update_Result':
+            connect = sqlite3.connect('KompoDB.db')
+            cursor = connect.cursor()
+            bot.send_message(message.chat.id, 'Таблица результатов обновленна, переведите в базу данных для скачавания')
+            cursor.execute("""DELETE FROM total_result
+                        INSERT INTO total_result
+                        select u.id as user_id,user_name, user_secondName,
+                            result_test1.score as result_test1,
+                            result_test2.score as result_test2,
+                            result_test3.score as result_test3,
+                            result_test4.score as result_test4,
+                            result_test5.score as result_test5,
+                            result_test6.score as result_test6,
+                            result_test7.score as result_test7,
+                            result_test8.score as result_test8,
+                            result_test9.score as result_test9,
+                            result_test10.score as result_test10,
+                            result_test11.score as result_test11,
+                            result_test12.score as result_test12,
+                            result_test13.score as result_test13,
+                            result_test14.score as result_test14,
+                            result_test15.score as result_test15,
+                            result_test16.score as result_test16,
+                            result_test17.score as result_test17,
+                            total_resultOffice.Total_Score as Total_ScoreOffice,
+                            total_resultMP.Total_Score as Total_ScoreMP,
+                            (total_resultOffice.Total_Score * 100 / 40)||'%' as ProgressOffice,
+                            (total_resultMP.Total_Score * 100 / 130)||'%' as ProgressMP,
+                            total_resultOffice.test_completed as TestCompletedOffice,
+                            total_resultMP.test_completed as TestCompletedMP,
+                            iif(total_resultOffice.test_completed=4, 1,0) as CompletedOfiiceCourse,
+                            iif(total_resultMP.test_completed=13, 1,0) as CompletedMPCourse,
+                            iif(total_resultOffice.Total_Score=40, 'Yes','NO') as CompletedOfiiceCourseSuccessfull,
+                            iif(total_resultMP.Total_Score=130, 'Yes','NO') as CompletedMPCourseSuccessfull
+                            from users u
+                            left JOIN users_result result_test1
+                            on u.id=result_test1.user_id
+                            and result_test1.test_number = 1
+                            left JOIN users_result result_test2
+                            on u.id=result_test2.user_id
+                            and result_test2.test_number = 2
+                            left JOIN users_result result_test3
+                            on u.id=result_test3.user_id
+                            and result_test3.test_number = 3
+                            left JOIN users_result result_test4
+                            on u.id=result_test4.user_id
+                            and result_test4.test_number = 4
+                            left JOIN users_result result_test5
+                            on u.id=result_test5.user_id
+                            and result_test5.test_number = 5
+                            left JOIN users_result result_test6
+                            on u.id=result_test6.user_id
+                            and result_test6.test_number = 6
+                            left JOIN users_result result_test7
+                            on u.id=result_test7.user_id
+                            and result_test7.test_number = 7
+                            left JOIN users_result result_test8
+                            on u.id=result_test8.user_id
+                            and result_test8.test_number = 8
+                            left JOIN users_result result_test9
+                            on u.id=result_test9.user_id
+                            and result_test9.test_number = 9
+                            left JOIN users_result result_test10
+                            on u.id=result_test10.user_id
+                            and result_test10.test_number = 10
+                            left JOIN users_result result_test11
+                            on u.id=result_test11.user_id
+                            and result_test11.test_number = 11
+                            left JOIN users_result result_test12
+                            on u.id=result_test12.user_id
+                            and result_test12.test_number = 12
+                            left JOIN users_result result_test13
+                            on u.id=result_test13.user_id
+                            and result_test13.test_number = 13
+                            left JOIN users_result result_test14
+                            on u.id=result_test14.user_id
+                            and result_test14.test_number = 14
+                            left JOIN users_result result_test15
+                            on u.id=result_test15.user_id
+                            and result_test15.test_number = 15
+                            left JOIN users_result result_test16
+                            on u.id=result_test16.user_id
+                            and result_test16.test_number = 16
+                            left JOIN users_result result_test17
+                            on u.id=result_test17.user_id
+                            and result_test17.test_number = 17
+                            left JOIN (
+                            Select user_id, sum(score) as Total_Score,
+                            sum(case when ifnull(score,0) > 0 then 1 else 0 END) as test_completed  from users_result
+                            WHERE test_number<=4
+                            GROUP by user_id
+                            ) total_resultOffice
+                            on u.id=total_resultOffice.user_id
+                            left JOIN (
+                            Select user_id, sum(score) as Total_Score,
+                            sum(case when ifnull(score,0) > 0 then 1 else 0 END) as test_completed  from users_result
+                            WHERE test_number>4 AND test_number<=17
+                            GROUP by user_id
+                            ) total_resultMP
+                            on u.id=total_resultMP.user_id
+                    """)
+            bot.send_message(message.chat.id, 'Таблица результатов обновленна, переведите в базу данных для скачавания')
 bot.polling(none_stop=True)
